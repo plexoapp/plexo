423f498374c4a91b85229cd8d655dcf9bec59eb3
Bregy Malpartida <bregy@minsky.cc>
2024-03-26 23:55:55 -0500

    feat: add SQL query and apply changes in main.rs

--- crates/plexo-next/src/main.rs
+++ crates/plexo-next/src/main.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 use git2::Repository;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let path = ".";
+    let path = "./playground/self";
     let repo = Repository::open(path)?;
 
     let branches = repo.branches(None).unwrap();
@@ -16,6 +16,15 @@ fn main() -> Result<(), Box<dyn Error>> {
             .collect::<Vec<_>>()
             .join("\n")
     );
+
+    let diff = repo.diff_index_to_workdir(None, None)?;
+    let location = git2::ApplyLocation::WorkDir;
+
+    // let options = git2::ApplyOptions::new();
+
+    let res = repo.apply(&diff, location, None);
+
+    println!("Apply result: {:?}", res);
 
     Ok(())
 }
