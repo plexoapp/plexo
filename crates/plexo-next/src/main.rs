90d93ab73187f82f32379a41fed04be6d6c4f947
Bregy Malpartida <bregy@minsky.cc>
2024-03-28 18:32:59 -0500

    feat: add dependencies async-openai, dotenv, and tokio

--- crates/plexo-next/src/main.rs
+++ crates/plexo-next/src/main.rs
@@ -1,8 +1,21 @@
-use std::error::Error;
+use async_openai::{
+    types::{
+        ChatCompletionRequestAssistantMessageArgs, ChatCompletionRequestSystemMessageArgs,
+        ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequestArgs,
+    },
+    Client,
+};
+use dotenv::dotenv;
+use git2::{Buf, Diff, DiffDelta, DiffHunk, DiffLine, DiffOptions, Repository};
+use std::{error::Error, io::BufWriter};
+use std::{io::Write, str};
 
-use git2::Repository;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
 
-fn main() -> Result<(), Box<dyn Error>> {
+    let client = Client::new();
+
     let path = "./playground/self";
     let repo = Repository::open(path)?;
 
@@ -17,14 +30,66 @@ fn main() -> Result<(), Box<dyn Error>> {
             .join("\n")
     );
 
+    // Diff::from_buffer(buffer)
+
     let diff = repo.diff_index_to_workdir(None, None)?;
-    let location = git2::ApplyLocation::WorkDir;
+
+    println!("deltas: \n{:?}\n", diff.deltas().collect::<Vec<_>>());
+
+    println!("Diff: {:?}", diff.stats()?);
+
+    let location = git2::ApplyLocation::Index;
 
     // let options = git2::ApplyOptions::new();
 
-    let res = repo.apply(&diff, location, None);
+    let request = CreateChatCompletionRequestArgs::default()
+        .max_tokens(512u16)
+        .model("gpt-3.5-turbo")
+        .messages([
+            ChatCompletionRequestSystemMessageArgs::default()
+                .content("You are a helpful assistant.")
+                .build()?
+                .into(),
+            ChatCompletionRequestUserMessageArgs::default()
+                .content("Who won the world series in 2020?")
+                .build()?
+                .into(),
+            ChatCompletionRequestAssistantMessageArgs::default()
+                .content("The Los Angeles Dodgers won the World Series in 2020.")
+                .build()?
+                .into(),
+            ChatCompletionRequestUserMessageArgs::default()
+                .content("Where was it played?")
+                .build()?
+                .into(),
+        ])
+        .build()?;
 
-    println!("Apply result: {:?}", res);
+    // let mut diff_print_result = BufWriter::new(Vec::new());
+
+    // diff.print(format, cb)
+
+    // println!("diff: {}", str::from_utf8(diff_print_result.get_ref()).unwrap());
+
+    let response = client.chat().create(request).await?;
+
+    println!("response: {:?}", response.choices.first().unwrap().message.content);
+
+    // let res = repo.apply(&diff, location, None);
+
+    // println!("Apply result: {:?}", res);
 
     Ok(())
+}
+
+fn print_diff_line(_delta: DiffDelta, _hunk: Option<DiffHunk>, line: DiffLine, buff: &mut BufWriter<Vec<u8>>) -> bool {
+    match line.origin() {
+        '+' | '-' | ' ' => print!("{}", line.origin()),
+        _ => {}
+    }
+    let l = str::from_utf8(line.content()).unwrap().to_string();
+
+    buff.write_all(l.as_bytes()).unwrap();
+
+    true
 }
