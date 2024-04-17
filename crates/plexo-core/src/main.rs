d542f46092fec5f71a9165c9b8fabdfe9f534b7a
Bregy Malpartida <bregy@minsky.cc>
2024-04-17 06:08:43 -0500

    feat: refactor main.rs to optimize route handling and remove commented out code

--- crates/plexo-core/src/main.rs
+++ crates/plexo-core/src/main.rs
@@ -8,25 +8,27 @@ use plexo_core::{
     },
     handlers::{graphiq_handler, graphql_handler, version_handler, ws_switch_handler},
 };
-use poem::{get, listener::TcpListener, middleware::Cors, post, EndpointExt, Route, Server};
+use poem::{get, handler, listener::TcpListener, middleware::Cors, post, EndpointExt, Route, Server};
+// use shuttle_poem::ShuttlePoem;
 use std::{error::Error, str::FromStr};
 use tracing::{info, subscriber::set_global_default, Level};
 use tracing_subscriber::FmtSubscriber;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    dotenv().ok();
-    set_global_default(
-        FmtSubscriber::builder()
-            .with_max_level(Level::from_str((*TRACING_LEVEL).to_uppercase().as_str()).unwrap_or(Level::INFO))
-            .finish(),
-    )
-    .expect("setting default subscriber failed");
+#[shuttle_runtime::main]
+async fn main(#[shuttle_runtime::Secrets] secrets: shuttle_runtime::SecretStore) -> ShuttlePoem<impl poem::Endpoint> {
+    // dotenv().ok();
+    secrets.into_iter().for_each(|(k, v)| std::env::set_var(k, v));
+    // set_global_default(
+    //     FmtSubscriber::builder()
+    //         .with_max_level(Level::from_str((*TRACING_LEVEL).to_uppercase().as_str()).unwrap_or(Level::INFO))
+    //         .finish(),
+    // )
+    // .expect("setting default subscriber failed");
 
-    let core = new_core_from_env().await?;
+    let core = new_core_from_env().await.unwrap();
     let engine_version = core.engine.version().unwrap();
 
-    let org = core.prelude().await?;
+    let org = core.prelude().await.unwrap();
 
     info!("welcome to {:?}", org.name);
     info!("version: {}", engine_version);
@@ -50,10 +52,40 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     info!("visit GraphQL Playground at {}/playground", *DOMAIN);
 
-    Server::new(TcpListener::bind(URL.to_owned()))
-        .run(app)
-        .await
-        .expect("Fail to start web server");
+    // Server::new(TcpListener::bind(URL.to_owned()))
+    //     .run(app)
+    //     .await
+    //     .expect("Fail to start web server");
 
-    Ok(())
+    // let app = Route::new().at("/", get(hello_world));
+
+    Ok(app.into())
 }
+
+pub struct PoemService<T>(pub T);
+
+#[shuttle_runtime::async_trait]
+impl<T> shuttle_runtime::Service for PoemService<T>
+where
+    T: poem::Endpoint + Send + 'static,
+{
+    async fn bind(mut self, addr: std::net::SocketAddr) -> Result<(), shuttle_runtime::Error> {
+        poem::Server::new(poem::listener::TcpListener::bind(addr))
+            .run(self.0)
+            .await
+            .map_err(shuttle_runtime::CustomError::new)?;
+
+        Ok(())
+    }
+}
+
+impl<T> From<T> for PoemService<T>
+where
+    T: poem::Endpoint + Send + 'static,
+{
+    fn from(router: T) -> Self {
+        Self(router)
+    }
+}
+
+pub type ShuttlePoem<T> = Result<PoemService<T>, shuttle_runtime::Error>;
